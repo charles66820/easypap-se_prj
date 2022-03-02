c8734e012e0ce069be4f8a4c600564e4ab28bbbc
Tanguy BERASATEGUY <tanguy.legeek@gmail.com>
2022-03-02 21:53:38 +0100

    plots cp taskloop && omp/taskloop

---
+++ plots/run-xp-ssand-omp-taskloop.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from expTools import *
+
+easypapOptions = {
+    "-k ": ["ssandPile"],
+    "-i ": [10],
+    "-v ": ["omp"],
+    "-s ": [512],
+    "-ts ": [8, 16, 32],
+    "-of ": ["ssand-omp.csv"]
+}
+
+# OMP Internal Control Variable
+ompICV = {
+    "OMP_SCHEDULE=": ["static", "static,1", "dynamic"],
+    "OMP_NUM_THREADS=": [1] + list(range(2, 49, 4)) + [47, 48]
+}
+
+nbrun = 3
+# Lancement des experiences
+execute('./run ', ompICV, easypapOptions, nbrun, verbose=False, easyPath=".")
+
+del easypapOptions["-ts "]
+easypapOptions["-th "] = [1]
+easypapOptions["-tw "] = [32, 64, 128, 256, 512]
+
+#execute('./run ', ompICV, easypapOptions, nbrun, verbose=False, easyPath=".")
+
+# Lancement de la version seq avec le nombre de thread impose a 1
+
+easypapOptions = {
+    "-k ": ["ssandPile"],
+    "-i ": [10],
+    "-v ": ["omp_taskloop"],
+    "-s ": [512],
+    "-ts ": [8, 16, 32],
+    "-of ": ["ssand-taskloop.csv"]
+}
+ompICV = {
+    "OMP_SCHEDULE=": ["static", "static,1", "dynamic"],
+    "OMP_NUM_THREADS=": [1] + list(range(2, 49, 4)) + [47, 48]
+}
+execute('./run ', ompICV, easypapOptions, nbrun, verbose=False, easyPath=".")
---
+++ plots/run-xp-ssand-taskloop.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from expTools import *
+
+easypapOptions = {
+    "-k ": ["ssandPile"],
+    "-i ": [10],
+    "-v ": ["omp_taskloop"],
+    "-s ": [512],
+    "-ts ": [8, 16, 32],
+    "-of ": ["ssand-taskloop.csv"]
+}
+
+# OMP Internal Control Variable
+ompICV = {
+    "OMP_SCHEDULE=": ["static", "static,1", "dynamic"],
+    "OMP_NUM_THREADS=": [1] + list(range(2, 49, 4)) + [47, 48]
+}
+
+nbrun = 3
+# Lancement des experiences
+execute('./run ', ompICV, easypapOptions, nbrun, verbose=False, easyPath=".")
+
+del easypapOptions["-ts "]
+easypapOptions["-th "] = [1]
+easypapOptions["-tw "] = [32, 64, 128, 256, 512]
+
+#execute('./run ', ompICV, easypapOptions, nbrun, verbose=False, easyPath=".")
+
+# Lancement de la version seq avec le nombre de thread impose a 1
+
+easypapOptions = {
+    "-k ": ["ssandPile"],
+    "-i ": [10],
+    "-s ": [512],
+    "-of ": ["ssand-taskloop.csv"]
+}
+ompICV = {"OMP_NUM_THREADS=": [1]}
+execute('./run ', ompICV, easypapOptions, nbrun, verbose=False, easyPath=".")
